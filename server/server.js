3e0d102135cb94ec448cedcf90524d994a104553
Kyle Henderson <kthenderson83@gmail.com>
2019-06-21 16:03:42 -0500

    add header

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const pool = require('./modules/pool')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -9,6 +10,15 @@ app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 
+app.get('/api/movies', (req, res) => {
+    pool.query('SELECT * FROM "movies" ORDER BY "title";')
+        .then( result => {
+            res.send(result.rows);
+        })
+        .catch( error => {
+            console.log(error);
+        })
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
