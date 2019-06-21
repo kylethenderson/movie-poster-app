439a60602539e132d732197ea7b277a339174937
Kyle Henderson <kthenderson83@gmail.com>
2019-06-21 16:06:37 -0500

    add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,16 @@ import './App.css';
 // Components
 import Header from '../Header/Header'
 
+import Footer from '../Footer/Footer'
+
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
         <Header />
+
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h4>Prime Digital Academy - Caesar - Week 12</h4>
+        </footer>
+    )
+}
+
+export default Footer
